70df706ae128e357807633ee41371374e0b0adf1
David Darmon <gendarmon@gmail.com>
2019-11-17 14:54:14 -0500

    Update props.R
    Update ylim selection for gf_pop_props.

--- R/props.R
+++ R/props.R
@@ -1,4 +1,5 @@
-gf_pop_props = function(x, conf.level = 0.95, p = NULL){
+gf_pop_props = function(x, conf.level = 0.95, p = NULL, ylim = NULL){
+  
   alpha = 1 - conf.level
   ci.out = multinomialCI(x, alpha = alpha)
   
@@ -8,17 +9,21 @@ gf_pop_props = function(x, conf.level = 0.95, p = NULL){
     est = data.frame(phat = x / sum(x), ci.l = ci.out[, 1], ci.u = ci.out[, 2])
   }
   
+  if (is.null(ylim)){
+    ylim =  range(est)
+  }
+  
   if (!is.null(p)){
     g = gf_point(phat ~ 1:nrow(est), data = est, cex = 3) %>%
       gf_errorbar(ci.l + ci.u ~ 1:nrow(est), data = est) %>%
       gf_labs(x = 'Category Number', y = 'Population Proportion') %>%
-      gf_lims(y = c(0, 1)) %>%
-      gf_point(p ~ (1:nrow(est) + 0.1), col = 'red', pch = 2, cex = 2)
+      gf_point(p ~ (1:nrow(est) + 0.1), col = 'red', pch = 2, cex = 2) %>% 
+      gf_lims(y = ylim)
   }else{
     g = gf_point(phat ~ 1:nrow(est), data = est, cex = 3) %>%
       gf_errorbar(ci.l + ci.u ~ 1:nrow(est), data = est) %>%
       gf_labs(x = 'Category Number', y = 'Population Proportion') %>%
-      gf_lims(y = c(0, 1))
+      gf_lims(y = ylim)
   }
   print(g)
   
