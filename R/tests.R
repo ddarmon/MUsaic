34496096341e323bab61be988ecf9f1029b3971a
David Darmon <gendarmon@gmail.com>
2020-04-21 06:31:20 -0400

    Adding Fisher's transformation option.

--- R/tests.R
+++ R/tests.R
@@ -18,7 +18,7 @@ two.sample.t.test = function(xbar, ybar, sx, sy, nx, ny, null.diff = 0,
   if (length(alternative) == 3){
     alternative = "two.sided"
   }
-  
+
   alpha = 1 - conf.level
 
   xbar.se = sx/sqrt(nx); ybar.se = sy/sqrt(ny)
@@ -89,7 +89,7 @@ one.sample.t.test = function(xbar, s, n, mu0 = 0,
   if (length(alternative) == 3){
     alternative = "two.sided"
   }
-  
+
   alpha = 1 - conf.level
 
   nu = n - 1
@@ -138,59 +138,66 @@ one.sample.t.test = function(xbar, s, n, mu0 = 0,
 
 cor.test.exact = function(x, y, rho0 = 0,
                              alternative = c("two.sided", "less", "greater"),
-                             conf.level = 0.95){
+                             conf.level = 0.95, exact = TRUE){
+
+  if (exact == TRUE){
+    pc <- pcorr
+  }else{
+    pc <- pcorr.fisher
+  }
+
   if (length(alternative) == 3){
     alternative = "two.sided"
   }
-  
+
   n <- length(x)
-  
+
   stopifnot(length(x) == length(y))
-  
+
   alpha = 1 - conf.level
-  
+
   r <- cor(x, y)
-  
+
   # Fudge factor here:
   # lower.eval <- -1+1e-3
   # upper.eval <- 1-1e-3
-  
+
   approx.int <- cor.test(x, y, conf.level = conf.level, alternative = alternative)$conf.int
-  
+
   if (alternative == 'two.sided'){
-    p.value = 2*min(pcorr(r, rho0, n), pcorr(r, rho0, n, lower.tail = FALSE))
-    
-    # lb <- uniroot(function(rho) pcorr(r, rho, n, lower.tail = FALSE) - alpha/2, interval = c(lower.eval, upper.eval))$root
-    # ub <- uniroot(function(rho) pcorr(r, rho, n) - alpha/2, interval = c(lower.eval, upper.eval))$root
-    
-    lb <- newtonRaphson(function(rho) pcorr(r, rho, n, lower.tail = FALSE) - alpha/2, x0 = approx.int[1])$root
-    ub <- newtonRaphson(function(rho) pcorr(r, rho, n) - alpha/2, x0 = approx.int[2])$root
-    
+    p.value = 2*min(pc(r, rho0, n), pc(r, rho0, n, lower.tail = FALSE))
+
+    # lb <- uniroot(function(rho) pc(r, rho, n, lower.tail = FALSE) - alpha/2, interval = c(lower.eval, upper.eval))$root
+    # ub <- uniroot(function(rho) pc(r, rho, n) - alpha/2, interval = c(lower.eval, upper.eval))$root
+
+    lb <- newtonRaphson(function(rho) pc(r, rho, n, lower.tail = FALSE) - alpha/2, x0 = approx.int[1])$root
+    ub <- newtonRaphson(function(rho) pc(r, rho, n) - alpha/2, x0 = approx.int[2])$root
+
     conf.int = c(lb, ub)
-    
+
     alt.text = sprintf("true correlation is not equal to %g", rho0)
   }else if (alternative == 'less'){
-    p.value = pcorr(r, rho0, n)
-    
-    # ub <- uniroot(function(rho) pcorr(r, rho, n) - alpha, interval = c(lower.eval, upper.eval))$root
-    ub <- newtonRaphson(function(rho) pcorr(r, rho, n) - alpha, x0 = approx.int[2])$root
-    
+    p.value = pc(r, rho0, n)
+
+    # ub <- uniroot(function(rho) pc(r, rho, n) - alpha, interval = c(lower.eval, upper.eval))$root
+    ub <- newtonRaphson(function(rho) pc(r, rho, n) - alpha, x0 = approx.int[2])$root
+
     conf.int = c(-1,ub)
-    
+
     alt.text = sprintf("true correlation is less than %g", rho0)
   }else if (alternative == 'greater'){
-    p.value = pcorr(r, rho0, n, lower.tail = FALSE)
-    
-    # lb <- uniroot(function(rho) pcorr(r, rho, n, lower.tail = FALSE) - alpha, interval = c(lower.eval, upper.eval))$root
-    lb <- newtonRaphson(function(rho) pcorr(r, rho, n, lower.tail = FALSE) - alpha, x0 = approx.int[1])$root
-    
+    p.value = pc(r, rho0, n, lower.tail = FALSE)
+
+    # lb <- uniroot(function(rho) pc(r, rho, n, lower.tail = FALSE) - alpha, interval = c(lower.eval, upper.eval))$root
+    lb <- newtonRaphson(function(rho) pc(r, rho, n, lower.tail = FALSE) - alpha, x0 = approx.int[1])$root
+
     conf.int = c(lb,1)
-    
+
     alt.text = sprintf("true correlation is greater than %g", rho0)
   }
-  
+
   attr(conf.int, which = "conf.level") = conf.level
-  
+
   names(r) <- "r"
   wtt <- list(method = "Exact bivariate Gaussian correlation test",
               data.name = paste(deparse(substitute(x)), "and", deparse(substitute(y))),
