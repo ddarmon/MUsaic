0f6351729ff2873099d476e69138ccb191e157a5
David Darmon <gendarmon@gmail.com>
2021-12-19 14:36:14 -0500

    Add convert.decimal.fraction.to.nary() to convery a decimal fraction to its base-n representation.

--- R/carithmetic.R
+++ R/carithmetic.R
@@ -20,6 +20,14 @@ integer.to.integer.power <- function(base, exponent){
   return(pow)
 }
 
+#' Get out the leading (left-most) digit in the base-10
+#' representation of an integer / float.
+#'
+#' @param number an integer / float
+first.decimal.digit <- function(number){
+  floor(number / 10^floor(log10(number)))
+}
+
 #' Convert a decimal fraction into a binary fraction using
 #' integer arithmetic with Horner's method.
 #'
@@ -102,14 +110,13 @@ convert.decimal.fraction.to.binary <- function(d, e, ndigits = 54){
     if (log10(b) >= e) {
       lead <- 1L
       a <- b - integer.to.integer.power(10L, e)
-    }
-    else {
+    } else {
       lead <- 0L
       a <- b
     }
     expansion[i] <- lead
 
-    # Find where the remainder has been seen, if it all.
+    # Find where the remainder has been seen, if at all.
 
     where.seen <- which(as == a)
 
@@ -177,4 +184,167 @@ convert.decimal.fraction.to.binary <- function(d, e, ndigits = 54){
          d)
 
   return(list(decimal.representation = decimal.representation, binary.representation = binary.representation, expansion = expansion, initial.part = initial.part, repeating.part = repeating.part, terms = as))
+}
+
+#' Convert a decimal fraction into its base-n (n-nary) representation using
+#' integer arithmetic with Horner's method.
+#'
+#' A function to convert a decimal fraction into its base-n
+#' (n-ary) representation.
+#'
+#' See
+#'
+#' http://cs.furman.edu/digitaldomain/more/ch6/dec_frac_to_bin.htm
+#'
+#' for more details.
+#'
+#' **NOTE:** d, e, and base should be **integer type**, i.e.
+#' suffixed with an `L`.
+#'
+#' If not, they will be coerced to integer type if possible.
+#'
+#' @param d the integer part of the decimal fraction.
+#' @param e the exponent for 10^(-e)
+#' @param base the base for the n-ary representation of the decimal fraction
+#' @param ndigits the maximum number of digits to search for the repeating pattern
+#'
+#' @examples
+#' # Binary expansion of 1/10 = 1/10^1
+#'
+#' convert.decimal.fraction.to.nary(1, 1, base = 2)
+#'
+#' # Trinary expansion of 1/10 = 1/10^1
+#'
+#' convert.decimal.fraction.to.nary(5, 1, base = 3)
+#'
+#' # Base-5 expansion of 1/5 = 2/10^1
+#'
+#' convert.decimal.fraction.to.nary(2, 1, base = 5)
+#' @export
+convert.decimal.fraction.to.nary <- function(d, e, base, ndigits = 54){
+  # Only works for decimal fractions.
+  stopifnot(e >= 1)
+  
+  # Make sure d < 10^e
+  
+  stopifnot(d < 10^e)
+  
+  # Check that d, e, and base are integer type.
+  # Otherwise convert to integer type.
+  
+  if (!is.integer(d)){
+    stopifnot(all.equal(d - floor(d), 0L))
+    
+    d <- as.integer(d)
+  }
+  
+  if (!is.integer(e)){
+    stopifnot(all.equal(e - floor(e), 0L))
+    
+    e <- as.integer(e)
+  }
+  
+  if (!is.integer(base)){
+    stopifnot(all.equal(base - floor(base), 0L))
+    
+    base <- as.integer(base)
+  }
+  
+  # Collector for the remainders from
+  #
+  #     base*d mod 10^e
+  #
+  # for finding when the remainder repeats (if it does).
+  
+  as <- c()
+  
+  a <- d
+  as <- c(as, a)
+  
+  # Collector for the final string-version of the n-ary
+  # expansion.
+  
+  nary.representation <- ''
+  
+  expansion <- rep(0, ndigits)
+  for (i in 1:ndigits) {
+    b <- base * a
+    
+    if (log10(b) >= e) {
+      lead <- first.decimal.digit(b)
+      a <- b %% integer.to.integer.power(10L, e)
+    } else {
+      lead <- 0L
+      a <- b
+    }
+    expansion[i] <- lead
+    
+    # Find where the remainder has been seen, if at all.
+    
+    where.seen <- which(as == a)
+    
+    if (length(where.seen) > 0){ # We are repeating, so can truncate and give the repeating part
+      
+      expansion <- expansion[1:i]
+      
+      if (where.seen == 1){ # The repeating part starts immediately after the decimal
+        nary.representation <- paste0("0.(",
+                                      paste0(expansion[where.seen:i], collapse = ''),
+                                      ")", collapse = '')
+        
+        initial.part <- c()
+        repeating.part <- expansion[where.seen:i]
+      }else{ # There are non-repeating parts before the decimal
+        nary.representation <- paste0("0.",
+                                      paste0(expansion[1:(where.seen-1)], collapse = ''),
+                                      "(",
+                                      paste0(expansion[where.seen:i], collapse = ''),
+                                      ")", collapse = '')
+        
+        initial.part <- expansion[1:(where.seen-1)]
+        repeating.part <- expansion[where.seen:i]
+      }
+      
+      break
+    }
+    
+    as <- c(as, a)
+    
+    if (identical(a, 0L)) { # Check if the n-ary expansion has terminated.
+      expansion <- expansion[1:i]
+      nary.representation <- paste0("0.", expansion, collapse = '')
+      
+      initial.part <- expansion[1:i]
+      repeating.part <- c()
+      
+      break
+    }
+  }
+  
+  if (nary.representation == ''){ # If the n-ary expansion hasn't repeated / terminated, indicate that
+    # the n-ary expansion hasn't been found by terminal '(...)'
+    nary.representation <- paste0("0.",
+                                  paste0(expansion, collapse = ''),
+                                  '(...)',
+                                  collapse = '')
+    
+    initial.part <- expansion
+    repeating.part <- c()
+  }
+  
+  # Get out decimal representation
+  # 
+  # NOTE: 
+  # 
+  #   d x 10^(-e) where d in 0, 1, 2, ..., 10^{e} - 1
+  # 
+  # log10(d) - e
+  
+  num.leading.zeros <- ceiling(e - log10(d)) - 1
+  
+  decimal.representation <- paste0('0.',
+                                   paste0(rep(0, num.leading.zeros), collapse = ''),
+                                   d)
+  
+  return(list(base = base, decimal.representation = decimal.representation, nary.representation = nary.representation, expansion = expansion, initial.part = initial.part, repeating.part = repeating.part, terms = as))
 }
